45b7b694210d9453439b545e2fd2a60949569131
ponyfly6 <2083323301@qq.com>
2026-04-17 10:16:34 +0800

    fix openai max_output_tokens validation

--- xpertai/models/openai/jest.config.ts
+++ xpertai/models/openai/jest.config.ts
@@ -1,9 +1,18 @@
+/**
+ * adds a docblock pointing Jest's loader at the dedicated ts config for this file.
+ *
+ * @jest-config-loader-options {"project":"tsconfig.jest.json"}
+ */
 /* eslint-disable */
 import { readFileSync } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const configDir = dirname(fileURLToPath(import.meta.url));
 
 // Reading the SWC compilation config for the spec files
 const swcJestConfig = JSON.parse(
-  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
+  readFileSync(join(configDir, '.spec.swcrc'), 'utf-8')
 );
 
 // Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
@@ -16,7 +25,10 @@ export default {
   transform: {
     '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
   },
-  transformIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ['/node_modules/(?!(lodash-es)/)'],
+  moduleNameMapper: {
+    '^lodash-es$': '<rootDir>/../../test-utils/lodashEsMock.ts'
+  },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: 'test-output/jest/coverage',
   testTimeout: 30000,
